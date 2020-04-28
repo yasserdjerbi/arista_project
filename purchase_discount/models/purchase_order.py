12a349db25288935b31943e7073908db38970f7e
DavidRudi <rudidavid@yahoo.com>
2020-04-28 15:22:20 +0700

    [ADD] add new module purchase_discount

---
+++ purchase_discount/models/purchase_order.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# Copyright 2004-2009 Tiny SPRL (<http://tiny.be>).
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2015-2017 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    @api.depends('discount')
+    def _compute_amount(self):
+        for line in self:
+            price_unit = False
+            # This is always executed for allowing other modules to use this
+            # with different conditions than discount != 0
+            price = line._get_discounted_price_unit()
+            if price != line.price_unit:
+                # Only change value if it's different
+                price_unit = line.price_unit
+                line.price_unit = price
+            super(PurchaseOrderLine, line)._compute_amount()
+            if price_unit:
+                line.price_unit = price_unit
+
+    discount = fields.Float(
+        string='Discount (%)', digits='Discount',
+    )
+
+    _sql_constraints = [
+        ('discount_limit', 'CHECK (discount <= 100.0)',
+         'Discount must be lower than 100%.'),
+    ]
+
+    def _get_discounted_price_unit(self):
+        """Inheritable method for getting the unit price after applying
+        discount(s).
+
+        :rtype: float
+        :return: Unit price after discount(s).
+        """
+        self.ensure_one()
+        if self.discount:
+            return self.price_unit * (1 - self.discount / 100)
+        return self.price_unit
+
+    def _get_stock_move_price_unit(self):
+        """Get correct price with discount replacing current price_unit
+        value before calling super and restoring it later for assuring
+        maximum inheritability.
+        """
+        price_unit = False
+        price = self._get_discounted_price_unit()
+        if price != self.price_unit:
+            # Only change value if it's different
+            price_unit = self.price_unit
+            self.price_unit = price
+        price = super(PurchaseOrderLine, self)._get_stock_move_price_unit()
+        if price_unit:
+            self.price_unit = price_unit
+        return price
+
+    def _prepare_account_move_line(self, move):
+        vals = super(PurchaseOrderLine, self)._prepare_account_move_line(move)
+        vals['discount'] = self.discount
+        return vals
