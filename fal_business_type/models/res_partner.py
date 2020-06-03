5fbef4f095a22df1c0a1e33ded4ede74e9fd4b69
Randy Raharjo <randyraharjo@gmail.com>
2020-06-03 17:04:00 +0700

    [IMP] Business Type in Partner

--- fal_business_type/models/res_partner.py
+++ fal_business_type/models/res_partner.py
@@ -5,3 +5,4 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     is_business_partner = fields.Boolean('Business Partner')
+    fal_business_type = fields.Many2one('fal.business.type', 'Business Type', index=True, domain="[('company_id', '=', company_id)]")
