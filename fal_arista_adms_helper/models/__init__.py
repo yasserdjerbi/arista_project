eab481d1dd44be1b528ca33ef2f26cdb0ecd69ca
Randy Raharjo <randyraharjo@gmail.com>
2020-06-11 15:06:24 +0700

    [IMP] ADMS CUST Dimension, Reason, No Rangka

--- fal_arista_adms_helper/models/__init__.py
+++ fal_arista_adms_helper/models/__init__.py
@@ -4,3 +4,5 @@ from . import stock_picking
 from . import res_partner
 from . import fal_cust_vendor_group
 from . import fal_reason_code
+from . import fal_no_rangka
+from . import fal_product_dimension
