2477240cda0d2f8b8a696556f336b25d86e69827
Randy Raharjo <randyraharjo@gmail.com>
2020-06-11 15:18:34 +0700

    [IMP] ADMS CUST Dimension, Reason, No Rangka

--- fal_arista_adms_helper/models/fal_product_dimension.py
+++ fal_arista_adms_helper/models/fal_product_dimension.py
@@ -8,3 +8,42 @@ class ProductDimension(models.Model):
     _description = "Product Dimension (Account)"
 
     name = fields.Char("Name")
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    product_dimension_id = fields.Many2one('x_product_dimension', string='Product Dimension', copy=False)
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    product_dimension_id = fields.Many2one('x_product_dimension', string='Product Dimension', copy=False)
+
+    def _prepare_account_move_line(self, move):
+        vals = super(PurchaseOrderLine, self)._prepare_account_move_line(move)
+        vals['product_dimension_id'] = self.product_dimension_id
+        return vals
+
+    def _prepare_invoice_line(self):
+        res = super(PurchaseOrderLine, self)._prepare_invoice_line()
+        res['product_dimension_id'] = self.product_dimension_id and self.product_dimension_id.id or False
+        return res
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    product_dimension_id = fields.Many2one('x_product_dimension', string='Product Dimension', copy=False)
+
+    def _prepare_invoice_line(self):
+        """
+        Prepare the dict of values to create the new invoice line for a sales order line.
+
+        :param qty: float quantity to invoice
+        """
+        self.ensure_one()
+        res = super(SaleOrderLine, self)._prepare_invoice_line()
+        res['product_dimension_id'] = self.product_dimension_id and self.product_dimension_id.id or False
+        return res
