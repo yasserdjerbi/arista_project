3ba41ee9562888de062d16466444de3bff8789e2
Randy Raharjo <randyraharjo@gmail.com>
2020-06-21 16:01:42 +0700

    [FIX] Akun Discount Product Categ

--- fal_arista_adms_helper/models/product.py
+++ fal_arista_adms_helper/models/product.py
@@ -4,7 +4,7 @@ from lxml.builder import E
 
 
 class ProductCategory(models.Model):
-    _name = 'product.category'
+    _inherit = 'product.category'
 
     property_account_discount_categ_id = fields.Many2one('account.account', company_dependent=True,
         string="Discount Account",
