981740398cf90c4af004af5d82f02488b97d3400
Randy Raharjo <randyraharjo@gmail.com>
2020-06-22 16:17:53 +0700

    [IMP] Stock Account ADMS

--- fal_arista_adms_helper/models/product.py
+++ fal_arista_adms_helper/models/product.py
@@ -9,3 +9,12 @@ class ProductCategory(models.Model):
     property_account_discount_categ_id = fields.Many2one('account.account', company_dependent=True,
         string="Discount Account",
         help="This account will be used when there is a discount for customer invoice.")
+    property_stock_account_transit_categ_id = fields.Many2one('account.account', company_dependent=True,
+        string="Transit Account",
+        help="This account will be used when there is a transfer.")
+    property_stock_account_output_input_main_categ_id = fields.Many2one('account.account', company_dependent=True,
+        string="Output Input to Main Account",
+        help="This account will be used when there is a transfer between main - branch.")
+    property_stock_account_output_input_branch_categ_id = fields.Many2one('account.account', company_dependent=True,
+        string="Output Input to Branch Account",
+        help="This account will be used when there is a transfer between branch - branch.")
